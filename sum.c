94c3aa8937b9979dfc6b927dbe47dc0b29a9731b
maheshkalse <42692116+maheshkalse@users.noreply.github.com>
2018-08-25 14:21:51 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 void main()
 {
-int n1,n2,add;
+int n1,n2;
 printf("enter the value of the two numbers");
 scanf("%d%d",&n1,&n2);
-add=n1+n2;
-printf("the addition is %d",add);
+printf("the addition is %d",n1+n2);
 }
